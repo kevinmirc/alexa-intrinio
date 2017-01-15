367938640d63b560dee8a2248fd37c16bd0ba842
Kevin Mircovich <kmircovich1@gmail.com>
2017-01-15 18:42:22 -0500

    testing seach response for alexa requests

--- app.js
+++ app.js
@@ -6,35 +6,36 @@ var app = koa();
 var router = require('koa-route');
 var intrinio = require('./services/intrinio');
 var dataPointMapping = require('./services/dataPointMapping');
+var alexa = require('./services/alexa');
 
 app
   .use(router.get('/', function *(next) {
     var _ = require('lodash');
 
     if (this.query.companyName && this.query.dataPoint) {
-      var res = yield intrinio.queryCompany(this.query.companyName);
-      var companies = JSON.parse(res).data;
+      var companyRes = yield intrinio.queryCompany(this.query.companyName);
+      var companies = JSON.parse(companyRes).data;
       var company = _.head(companies);
 
       if (company) {
         // convert the human readable dataPoint string to an intrinio-api friendly datapoint string
         var dataPoint = dataPointMapping.find(this.query.dataPoint);
         if (dataPoint) {
-          this.body = yield intrinio.getDataPoint(company.ticker, dataPoint.intrinioDataPoint);
+          var dataPointRes = yield intrinio.getDataPoint(company.ticker, dataPoint.intrinioDataPoint);
+          // dataPointRes = JSON.parse(dataPointRes);
+          this.body = alexa.buildResponseBody(dataPointRes.value, 'speachType', 'Unit');
         } else {
           console.warn(`Unmatched data point query: '${this.query.dataPoint}'`);
           this.statusCode = 404;
-          this.body = {error: `Could not find ${this.query.dataPoint} for ${this.query.companyName}`};
+          this.body = alexa.buildResponseBody(`Could not find ${this.query.dataPoint} for ${this.query.companyName}`);
         }
-        // return speach string
-        // return speach string with unit as string ex: "dollars", "shares", "percent", etc.
       } else {
         this.statusCode = 404;
-        this.body = {error: `Could not find a company with name: ${this.query.companyName}`};
+        this.body = alexa.buildResponseBody(`Could not find a company with name: ${this.query.companyName}`);
       }
     } else {
       this.statusCode = 400;
-      this.body = {error: `Must provide query for companyName and dataPoint.`};
+      this.body = alexa.buildResponseBody(`Must provide query for companyName and dataPoint.`);
     }
     yield next;
   }));
---
+++ services/alexa.js
@@ -0,0 +1,34 @@
+/* jshint esversion: 6 */
+/*
+response body should look like this:
+<speak>
+    Here is a number spoken as a cardinal number:
+    <say-as interpret-as="cardinal">12345</say-as>.
+    Here is the same number with each digit spoken separately:
+    <say-as interpret-as="digits">12345</say-as>.
+    Here is a word spelled out: <say-as interpret-as="spell-out">hello</say-as>
+</speak>
+*/
+var _ = require('lodash');
+// var interpretAsOptions =
+// /*
+// haracters, spell-out: Spell out each letter.
+// cardinal, number: Interpret the value as a cardinal number.
+// ordinal: Interpret the value as an ordinal number.
+// digits: Spell each digit separately .
+// fraction: Interpret the value as a fraction. This works for both common fractions (such as 3/20) and mixed fractions (such as 1+1/2).
+// unit: Interpret a value as a measurement. The value should be either a number or fraction followed by a unit (with no space in between) or just a unit.
+// date: Interpret the value as a date. Specify the format with the format attribute.
+// time: Interpret a value such as 1'21" as duration in minutes and seconds.
+// telephone: Interpret a value as a 7-digit or 10-digit telephone number. This can also handle extensions (for example, 2025551212x345).
+// address: Interpret a value as part of street address.
+// */
+// [haracters, spell-out, cardinal, number, ordinal, digits, fraction, unit, date, time, telephone, address];
+
+module.exports = {
+  // return speach string
+  // return speach string with unit as string ex: "dollars", "shares", "percent", etc.
+  buildResponseBody: function (text, speachType, unit) {
+    return `<speak>${text}</speak>`;
+  }
+};
